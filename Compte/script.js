8e104663a28d77304f74ce9bf0c5254fee06357c
Gael Winandy <gaelw@mac.com>
2016-02-03 18:34:39 +0100

    Compte

---
+++ Compte/script.js
@@ -0,0 +1,28 @@
+/* Calculette à dépense */
+app = angular.module('app', []);
+
+app.controller('appCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.depenses = [
+        { valeur: 10 },
+		{ valeur: 15 },
+		{ valeur: 20 },
+    ];
+    
+    $scope.addDepense = function() {
+        $scope.depenses.push({ valeur: parseInt($scope.valeur) });
+    }
+    
+    $scope.rmDepense = function(i) {
+        $scope.depenses.splice(i, 1);
+    }
+    
+    $scope.doTotal = function() {
+        total = 0;
+        for (i = 0 ; i < $scope.depenses.length ; i++) {
+            total += $scope.depenses[i].valeur;
+        }
+        $scope.total = total;
+    }
+    
+    $scope.$watch("depenses", $scope.doTotal, true)
+}]);
